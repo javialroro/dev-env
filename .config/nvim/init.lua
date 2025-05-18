795450fae3876a62cb6e59febb5330791b2633a7
Javier Rojas <102406440+javialroro@users.noreply.github.com>
2025-05-17 21:17:35 -0600

    ready-tmux mod

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,5 @@
 -- Making exec
 print("hello nvim")
 vim.keymap.set('n', '<C-e>', ':!chmod +x %<CR>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '<C-a>', 'ggVG"+y', { noremap = true, silent = true })
 
